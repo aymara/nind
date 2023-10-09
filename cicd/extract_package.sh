8f13e4b9a86f6602e01a76e3f7242b58e0599022
Benjamin Labbé <benjamin.labbe@cea.fr>
2023-10-09 16:46:39 +0200

    update script to extract package

--- cicd/extract_package.sh
+++ cicd/extract_package.sh
@@ -2,6 +2,7 @@
 
 dockerImage=$1
 packageDir=$2
+GITHUB_OUTPUT=$3
 id=$(docker run -d "$dockerImage" sleep infinity)
 echo -n "export fullname=$packageDir/" > ./envFile
 docker exec $id ls $packageDir >> ./envFile
@@ -11,6 +12,10 @@ echo "tmp=\${trimmed#*-}" >> ./envFile
 echo "export version=\${tmp%-*}" >> ./envFile
 echo "filename=$(. envFile && echo $filename)" >> $GITHUB_OUTPUT
 echo "version=$(. envFile && echo $version)"  >> $GITHUB_OUTPUT
+. envFile && echo "filename=$filename , version=$version"
+echo "Extract file from docker image"
 . envFile && docker cp $id:$fullname - > ./$filename
 docker stop $id
 docker rm -v $id
+echo "Finaly, the package should be present in local folder : "
+if [ -e "./$filename" ]; then echo "!! yes : ./$filename" ; else echo "!! no"; exit 1; fi
