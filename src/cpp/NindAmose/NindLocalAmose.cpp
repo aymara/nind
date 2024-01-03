aa7918eedb9cb5bb04411067a23cd7c97abade84
Benjamin Labbé <benjamin.labbe@cea.fr>
2024-01-03 18:47:36 +0100

    Avoid Exception in NindLocalAmose::getDocTerms() when term is not found in lexicon

--- src/cpp/NindAmose/NindLocalAmose.cpp
+++ src/cpp/NindAmose/NindLocalAmose.cpp
@@ -48,7 +48,7 @@ NindLocalAmose::~NindLocalAmose()
 //of terms (from @ref termIds) in documents (from @ref documents)
 //param termIds vector of identifier of terms
 //param documents vector of documents where to search for position of terms.
-//param positions position of occurrences of terms in documents. One element foreach content id in @ref documents.
+//param positions position of occurrences of terms in documents. One element foreach content  id in @ref documents.
 //Each element is a vector containing one element for each term in termIds.
 //And each of these elements is the list of positions and lengths of the occurrences of this term in this document.  */
 void NindLocalAmose::getTermPositionIndocs(const vector<unsigned int>& termIds,
@@ -114,7 +114,12 @@ bool NindLocalAmose::getDocTerms(const unsigned int docId,
         AmoseTypes type;
         string namedEntity;
         const bool trouvej = m_nindLexicon.getWord((*itterm), lemma, type, namedEntity);
-        if (!trouvej) throw IncompatibleFileException("Unknown term into lexicon");
+        if (!trouvej)
+        {
+          cerr<<"NindLocalAmose::getDocTerms(docId="<<docId<<") Unknown term in lexicon: NE='"<<namedEntity<<"' lemma='"<<lemma<<"'"<<endl;
+          //throw IncompatibleFileException("Unknown term in lexicon: NE='"<<namedEntity<<"' lemma='"+lemma+"'");
+          continue;
+        }
         if (type == termType)
         {
             if (type == NAMED_ENTITY)
