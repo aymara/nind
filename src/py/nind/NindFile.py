cfb01fcb870a2aa273d038e5c7f55e20a2fc1035
Gaël de Chalendar <gael.de-chalendar@cea.fr>
2026-09-15 02:02:03 +0200

    Bump version v2.0.4 -> v2.0.5

--- src/py/nind/NindFile.py
+++ src/py/nind/NindFile.py
@@ -3,7 +3,7 @@
 __author__ = "jys"
 __copyright__ = "Copyright (C) 2017 LATEJCON"
 __license__ = "GNU LGPL"
-__version__ = "2.0.4"
+__version__ = "2.0.5"
 # Author: jys <jy.sage@orange.fr>, (C) LATEJCON 2017
 # Copyright: 2014-2017 LATEJCON. See LICENCE.md file that comes with this distribution
 # This file is part of NIND (as "nouvelle indexation").
