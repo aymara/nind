48560a222b2892a56146aa861d01a3814ce30466
Gaël de Chalendar <gael.de-chalendar@cea.fr>
2026-09-15 11:11:30 +0200

    Bump version v2.1.1 -> v2.1.2

--- src/py/nind/NindRetrolexicon.py
+++ src/py/nind/NindRetrolexicon.py
@@ -17,7 +17,7 @@ exists to read files produced by the C++ side or by legacy nind tooling.
 __author__ = "jys"
 __copyright__ = "Copyright (C) 2017 LATEJCON"
 __license__ = "GNU LGPL"
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 # Author: jys <jy.sage@orange.fr>, (C) LATEJCON 2017
 # Copyright: 2014-2017 LATEJCON. See LICENCE.md file that comes with this distribution
 # This file is part of NIND (as "nouvelle indexation").
