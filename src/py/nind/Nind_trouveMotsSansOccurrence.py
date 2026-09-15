8052a3dffee915ae09564ece7041857020c7c09c
Gaël de Chalendar <gael.de-chalendar@cea.fr>
2026-09-15 02:09:10 +0200

    Bump version v2.0.6 -> v2.0.7

--- src/py/nind/Nind_trouveMotsSansOccurrence.py
+++ src/py/nind/Nind_trouveMotsSansOccurrence.py
@@ -3,7 +3,7 @@
 __author__ = "jys"
 __copyright__ = "Copyright (C) 2019 LATEJCON"
 __license__ = "GNU LGPL"
-__version__ = "2.0.6"
+__version__ = "2.0.7"
 # Author: jys <jy.sage@orange.fr>, (C) LATEJCON 2019
 # Copyright: 2014-2017 LATEJCON. See LICENCE.md file that comes with this distribution
 # This file is part of NIND (as "nouvelle indexation").
